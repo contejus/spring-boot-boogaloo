7b2ed69165bb774e3ee6eb0520fc243fe078843f
contejus <22385070+contejus@users.noreply.github.com>
2021-04-25 13:51:49 -0500

    add scheduling example

--- src/main/java/io/github/contejus/gettingstarted/GettingStartedApplication.java
+++ src/main/java/io/github/contejus/gettingstarted/GettingStartedApplication.java
@@ -4,12 +4,14 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableScheduling
 public class GettingStartedApplication {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
